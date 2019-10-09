0fbf041eaafe9d49f1cd1619ae607e6ce9758596
David Pribyl Pierdinock <dpribylp@nd.edu>
2019-10-09 15:46:13 -0400

    This should work

--- wagesAssignment.sh
+++ wagesAssignment.sh
@@ -1,5 +1,7 @@
 #manipulate data in wages.csv
 #usage: bash wagesAssignment.sh
 
-cat wages.csv | cut -d , -f 1,2 | sed 's/,/ /g' | sort -k 1 > genderExpCombos.txt
-cat genderExpCombos.txt | 
+cat wages.csv | cut -d , -f 1,2 | sed 's/,/ /g' | sort -k 1 | uniq  > genderExpCombos.txt
+cat genderExpCombos.txt | grep "female" | sort -k 2n > genderExp.txt
+cat genderExpCombos.txt | grep "^male" | sort -k 2n >> genderExp.txt  
+cat genderExp.txt
