9a8e8cb1742f055562a4699059a40786960991e5
David Pribyl Pierdinock <dpribylp@nd.edu>
2019-10-09 16:18:56 -0400

    First two parts of the assignment

--- wagesAssignment.sh
+++ wagesAssignment.sh
@@ -1,7 +1,18 @@
 #manipulate data in wages.csv
 #usage: bash wagesAssignment.sh
 
+#Returns unique gender and years of experience combos
+
 cat wages.csv | cut -d , -f 1,2 | sed 's/,/ /g' | sort -k 1 | uniq  > genderExpCombos.txt
 cat genderExpCombos.txt | grep "female" | sort -k 2n > genderExp.txt
 cat genderExpCombos.txt | grep "^male" | sort -k 2n >> genderExp.txt  
 cat genderExp.txt
+
+#Returns gender, experience, and wage of highest and lowest earner and the number of females in the top ten highest earners
+
+echo "Gender, years of experience, and wage of the highest earner"
+tail -n +2 wages.csv | cut -d , -f 1,2,4 | sort -k 3 | tail -n 1
+echo "Gender, years of experience, and wage of the lowest earner"
+tail -n +2 wages.csv | cut -d , -f 1,2,4 | sort -k 3 | head -n 1
+echo "The number of females in the top ten earners"
+tail -n +2 wages.csv | cut -d , -f 1,2,4 | sort -k 3 | tail -n 10 | grep -c "female" 
