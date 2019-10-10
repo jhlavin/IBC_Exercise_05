2127e0ea1d55e18335c4fa4457951ec0a13c18e6
John Hlavin <jhlavin@nd.edu>
2019-10-10 16:21:13 -0400

    Finished 3rd task and tested the code

--- wagesAssignment.sh
+++ wagesAssignment.sh
@@ -5,11 +5,11 @@
 
 #cat wages.csv | cut -d , -f 1,2 | sed 's/,/ /g' | sort -k 1 | uniq  > genderExpCombos.txt
 #cat genderExpCombos.txt | grep "female" | sort -k 2n > genderExp.txt
-#cat genderExpCombos.txt | grep "^male" | sort -k 2n >> genderExp.txt  
+#cat genderExpCombos.txt | grep "^male" | sort -k 2n >> genderExp.txt
 #cat genderExp.txt
 
 cat wages.csv | cut -d , -f 1,2 | sed 's/,/ /g' | sort -k 1 | uniq | grep "female" | sort -k 2n > genderExp.txt
-cat wages.csv | cut -d , -f 1,2 | sed 's/,/ /g' | sort -k 1 | uniq | grep "^male" | sort-k 2n >> genderExp.txt
+cat wages.csv | cut -d , -f 1,2 | sed 's/,/ /g' | sort -k 1 | uniq | grep "^male" | sort -k 2n >> genderExp.txt
 
 #Returns gender, experience, and wage of highest and lowest earner and the number of females in the top ten highest earners
 
@@ -18,10 +18,11 @@ tail -n +2 wages.csv | cut -d , -f 1,2,4 | sort -k 3 | tail -n 1
 echo "Gender, years of experience, and wage of the lowest earner"
 tail -n +2 wages.csv | cut -d , -f 1,2,4 | sort -k 3 | head -n 1
 echo "The number of females in the top ten earners"
-tail -n +2 wages.csv | cut -d , -f 1,2,4 | sort -k 3 | tail -n 10 | grep -c "female" 
+tail -n +2 wages.csv | cut -d , -f 1,2,4 | sort -k 3 | tail -n 10 | grep -c "female"
 
-#Returns the effect of graduating college on wage
+#Returns the amount by which the minimum wage of a college grad exceeds that of a HS grad
 
-echo "College graduates make this much more money than highschool graduates on average"
-cat wages.csv | grep ",12" | cut -d , -f 4 > highschool.txt
-cat wages.csv | grep ",16" | cut -d , -f 4 > college.txt
+echo "The minimum wage of a college graduate exceeds that of a high school graduate by this amount:"
+HSmin=$(grep ",12," wages.csv | cut -d , -f 4 | sort -n | head -n 1 | sed -e "s/\r//g")
+Cmin=$(grep ",16," wages.csv | cut -d , -f 4 | sort -n | head -n 1 | sed -e "s/\r//g")
+echo "$Cmin - $HSmin" | bc
